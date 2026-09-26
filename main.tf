8a55ada90827f5072b19309fb4bec76af5f41bfe
ssup2 <16958310+ssup2@users.noreply.github.com>
2026-09-26 18:43:45 +0900

    Use a single NAT gateway to cut idle cost

--- main.tf
+++ main.tf
@@ -69,6 +69,7 @@ module "vpc" {
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 3)]
 
   enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
